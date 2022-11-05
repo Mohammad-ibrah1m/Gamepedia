04521528056855dc9cea3835b06304abb294b268
bayankharosheh3 <bayankharosheh3@gmail.com>
2022-11-05 11:05:59 +0200

    component folder

---
+++ gamepedia/src/Components/Container/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Style from './styles.module.css'
+
+const Container = () => {
+  return (
+    <div style={Style.container}>Container</div>
+  )
+}
+
+export default Container
