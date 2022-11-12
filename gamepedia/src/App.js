fcce7178956a5a4a8a34877d697911102b6c9aa5
Mohammad-ibrah1m <mohammads.ibrahim0@gmail.com>
2022-11-12 14:48:32 +0200

    fixed Navbar background

--- gamepedia/src/App.js
+++ gamepedia/src/App.js
@@ -2,7 +2,6 @@ import logo from "./logo.svg";
 import "./App.css";
 import Container from "./Components/Container";
 import NavBar from "./Components/NavBar";
-import PopUp from "./Components/Modal/index.jsx";
 
 function App() {
   return (<><NavBar></NavBar></>)
